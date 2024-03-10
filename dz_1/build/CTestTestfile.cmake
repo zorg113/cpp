5e85d534a82cfa36f647af9451513d8ba15a86d8
dan <dmitryina@gmail.com>
2024-03-10 20:40:53 +0300

    fix: path to dz 6

--- dz_1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dan/otus/cpp_course/cpp/dz_1
-# Build directory: /home/dan/otus/cpp_course/cpp/dz_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
-set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/dan/otus/cpp_course/cpp/dz_1/CMakeLists.txt;85;add_test;/home/dan/otus/cpp_course/cpp/dz_1/CMakeLists.txt;0;")
--- dz_1/build/version.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define PROJECT_VERSION_PATCH 1
-
