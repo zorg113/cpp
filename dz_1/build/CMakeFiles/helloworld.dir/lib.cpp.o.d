4924dfc490ad557597fdd6c82d023ca9fda1c262
dan <dmitryina@gmail.com>
2024-03-10 20:28:30 +0300

    fix: path to dz 3

--- dz_1/build/CMakeFiles/helloworld.dir/lib.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/helloworld.dir/lib.cpp.o: \
- /home/dan/otus/cpp_course/cpp/dz_1/lib.cpp /usr/include/stdc-predef.h \
- /home/dan/otus/cpp_course/cpp/dz_1/lib.h \
- /home/dan/otus/cpp_course/cpp/dz_1/build/version.h
