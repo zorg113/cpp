847eb9e4b4358ef4ebc9ab15d096ca99a57caab1
dan <dmitryina@gmail.com>
2024-07-09 12:27:46 +0300

    fix: file hpp

---
+++ dz_7/ioutdev.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+
+#include "bulk.hpp"
+
+namespace cmd {
+/**
+ * @brief class outpud data
+ *
+ */
+class ioutdev {
+public:
+  virtual void update(const bulk &blk) = 0;
+};
+/**
+ * @brief  class output data to console
+ *
+ */
+class console : public ioutdev {
+public:
+  void update(const bulk &blk) final {
+    std::cout << "bulk: " << blk.output() << std::endl;
+  }
+};
+/**
+ * @brief class output data to file
+ *
+ */
+class file : public ioutdev {
+public:
+  void update(const bulk &blk) final {
+    std::ofstream f("bulk" + blk.name());
+    f << blk.output() << std::endl;
+    f.close();
+  }
+};
+
+} // namespace cmd
--- dz_8/include/bayan.hpp
+++ dz_8/include/bayan.hpp
@@ -1,19 +1,30 @@
 #pragma once
-#include <vector>
+#include <cstddef>
+#include <memory>
 #include <string>
+#include <vector>
 namespace ba {
 using str = std::string;
 using vec_str = std::vector<str>;
-/// @brief  configure bayan class 
+enum hash_type { _crc_, _sha256_ };
+/// @brief  configure bayan class
 struct configure {
-    vec_str m_targets;
-    vec_str m_excludes;
-
-}
-/// @brief  class serch file dublicate 
+  vec_str m_targets;
+  vec_str m_excludes;
+  vec_str m_patterns;
+  size_t m_depth = 0;
+  size_t m_minsize = 0;
+  hash_type m_hash = hash_type::_crc_;
+};
+/// @brief  class serch file dublicate
 class bayan {
-    public:
+public:
+  bayan(const configure &config);
+  configure &get_config() const { return }
 
+private:
+  configure m_conf;
+  std::unique_ptr<Hash> m_hash;
 };
 
 } // namespace ba
